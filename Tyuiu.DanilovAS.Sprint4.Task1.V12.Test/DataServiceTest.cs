3f9370f3bb346a44361f392e4f08659a04632a1c
Александр Данилов <Tyuiu.DanilovAS@outlook.com>
2025-10-17 14:00:35 +0500

    Task1

---
+++ Tyuiu.DanilovAS.Sprint4.Task1.V12.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.DanilovAS.Sprint4.Task1.V12.Lib;
+namespace Tyuiu.DanilovAS.Sprint4.Task1.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[] array = new int[13] { 4, 8, 6, 4, 9, 5, 8, 7, 8, 4, 7, 6, 8 };
+            var wait = 2205;
+            var result = ds.Calculate(array);
+            
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.DanilovAS.Sprint4.Task1.V12/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.DanilovAS.Sprint4.Task1.V12.Lib;
+namespace Tyuiu.DanilovAS.Sprint4.Task1.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Данилов А.С. | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема:                             *");
+            Console.WriteLine("* Задание #                                                              *");
+            Console.WriteLine("* Вариант #                                                              *");
+            Console.WriteLine("* Выполнил: Данилов Александр Сергеевич | ИИПб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 13 элементов заполненный         *");
+            Console.WriteLine("*  значениями с клавиатуры в диапазоне от 4 до 9 подсчитать произведение  *");
+            Console.WriteLine("*  нечетных элементов массива.                                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] array = new int[13];
+
+            for (int i = 0; i <= array.Length-1; i++)
+            {
+                Console.Write($"Введите эл-т массива {i + 1} => ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int result = ds.Calculate(array);
+            Console.WriteLine($"Ответ равен => {result}");
+            Console.ReadKey();
+        }
+    }
+}
