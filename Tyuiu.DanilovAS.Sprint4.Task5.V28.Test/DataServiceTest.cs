f36d16e38501f2c313227a62748d209c6d9ffa47
Александр Данилов <Tyuiu.DanilovAS@outlook.com>
2025-10-17 15:55:44 +0500

    Task5

---
+++ Tyuiu.DanilovAS.Sprint4.Task5.V28.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualBasic;
+using Tyuiu.DanilovAS.Sprint4.Task5.V28.Lib;
+namespace Tyuiu.DanilovAS.Sprint4.Task5.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix = new int[5, 5];
+
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+
+            int[,] waitMatrix = new int[5, 5] { { 4, 3, 2, 8, 7 },
+                                              { 0,0,0,0,0 },
+                                              { 3, 8, 8, 3, 5 },
+                                              { 5, 7, 5, 4, 4 },
+                                              { 8, 8, 3, 7, 6 } };
+
+            int[,] result = new int[5, 5]     { { 4, 3, 2, 8, 7 },
+                                              { -4, -5, -5, -5, -7 },
+                                              { 3, 8, 8, 3, 5 },
+                                              { 5, 7, 5, 4, 4 },
+                                              { 8, 8, 3, 7, 6 } };
+
+            result = ds.Calculate(result);
+            Collection.Equals(waitMatrix, result);
+        }
+    }
+}
---
+++ Tyuiu.DanilovAS.Sprint4.Task5.V28/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Numerics;
+using Tyuiu.DanilovAS.Sprint4.Task5.V28.Lib;
+namespace Tyuiu.DanilovAS.Sprint4.Task5.V28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Random random = new Random();
+
+            Console.Title = "Спринт #4 | Выполнил: Данилов А.С. | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы. (генератор случайных чисел)                    *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнил: Данилов Александр Сергеевич | ИИПб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* случайными значениями в диапазоне от -6 до 3. Заменить отрицательные    *");
+            Console.WriteLine("* элементы на 0.                                                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] array = new int[5, 5];
+
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length / rows;
+
+            for(int i = 0; i<rows; i++)
+            {
+                for(int j = 0; j<columns; j++)
+                {
+                    array[i, j] = random.Next(-6, 3);
+                }
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            array = ds.Calculate(array);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{array[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
