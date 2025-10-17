3cb5657f37630a03c784e754e917fdcf6fbf0c40
Александр Данилов <Tyuiu.DanilovAS@outlook.com>
2025-10-17 15:26:28 +0500

    Task4

---
+++ Tyuiu.DanilovAS.Sprint4.Task4.V30.Test/DataServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualBasic;
+using Tyuiu.DanilovAS.Sprint4.Task4.V30.Lib;
+namespace Tyuiu.DanilovAS.Sprint4.Task4.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix = new int[5, 5];
+
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+
+            int[,] waitMatrix = new int[5, 5] { { 4, 0, 2, 8, 0 },
+                                              { 4, 6, 0, 0, 0 },
+                                              { 0, 8, 8, 0, 5 },
+                                              { 0, 0, 0, 4, 4 },
+                                              { 8, 8, 0, 0, 6 } };
+
+            int[,] result = new int[5, 5]     { { 4, 3, 2, 8, 7 },
+                                              { 4, 6, 5, 5, 7 },
+                                              { 3, 8, 8, 3, 5 },
+                                              { 5, 7, 5, 4, 4 },
+                                              { 8, 8, 3, 7, 6 } };
+
+            result = ds.Calculate(result);
+
+            Collection.Equals(waitMatrix,result);
+        }
+    }
+}
---
+++ Tyuiu.DanilovAS.Sprint4.Task4.V30/Program.cs
@@ -0,0 +1,59 @@
+﻿using Tyuiu.DanilovAS.Sprint4.Task4.V30.Lib;
+namespace Tyuiu.DanilovAS.Sprint4.Task4.V30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт 4 | Выполнил: Данилов А.С. | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы. (ввод с клавиатуры)                            *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Выполнил: Данилов Александр Сергеевич | ИИПб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 3 до 7. Заменить нечетные        *");
+            Console.WriteLine("* элементы массива на 0                                                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] matrix = new int[5, 5];
+
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+
+            for (int i = 0; i<rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"[{i},{j}] = ");
+                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            matrix = ds.Calculate(matrix);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{matrix[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
